38235b157fd24d46a07aa1149ae3eab98ba79495
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-09-29 21:44:41 -0600

    Create global colors

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@ import "./style.css";
 const content = document.createElement("div");
 content.id = "content";
 
+// Header
+const headerContainer = document.createElement("header");
+const headerLogo = document.createElement("div");
+headerLogo.classList.add("logo-header");
+headerLogo.textContent = "Logo";
+
+headerContainer.appendChild(headerLogo);
+
+content.appendChild(headerContainer);
+
+// Append content to body
 document.body.appendChild(content);
