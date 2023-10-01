eb920a6fa2114634d722bf1909f6eb1c1df72610
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-10-01 07:55:59 -0600

    Initialize home content

---
+++ src/pages/home.js
@@ -0,0 +1,6 @@
+export default function renderHome() {
+  const home = document.createElement("div");
+  home.classList = "home";
+
+  return home;
+}
--- src/sections/content.js
+++ src/sections/content.js
@@ -1,6 +1,10 @@
+import renderHome from "../pages/home.js";
+
 export default function renderContent() {
   const content = document.createElement("div");
   content.id = "content";
+
+  content.appendChild(renderHome());
 
   document.body.appendChild(content);
 }
