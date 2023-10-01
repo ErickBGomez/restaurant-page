40606ea8bcacd1cf8f51cec4f8d93f814c68cb65
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-10-01 11:01:36 -0600

    Add black background with image to hero container

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,15 @@
+function createHero() {
+  const heroContainer = document.createElement("div");
+  heroContainer.className = "hero-bg";
+
+  return heroContainer;
+}
+
 export default function renderHome() {
   const home = document.createElement("div");
   home.classList = "home";
+
+  home.appendChild(createHero());
 
   return home;
 }
