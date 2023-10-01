6a5080b51c0bb5917bd0b1979feac0f90ded99c1
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-10-01 07:15:03 -0600

    Add header to bundle output

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    header: "./src/pages/header.js",
+  },
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
   },
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
