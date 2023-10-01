f48a64381b4283291b74f98136547c7c77706b85
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-10-01 11:39:10 -0600

    Add button styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,30 @@
 function createHero() {
   const heroContainer = document.createElement("div");
-  heroContainer.className = "hero-bg";
+  heroContainer.className = "hero-container";
+
+  const heroText = document.createElement("div");
+
+  const heroTitle = document.createElement("h2");
+  heroTitle.textContent = "Welcome to ";
+  const restaurantTitleSpan = document.createElement("span");
+  restaurantTitleSpan.textContent = "TITLE RESTAURANT";
+  heroTitle.appendChild(restaurantTitleSpan);
+
+  const heroSubtitle = document.createElement("h1");
+  heroSubtitle.textContent = "Enjoy the best of ";
+  const countrySubtitleSpan = document.createElement("span");
+  countrySubtitleSpan.textContent = "El Salvador";
+
+  heroSubtitle.appendChild(countrySubtitleSpan);
+
+  heroText.appendChild(heroTitle);
+  heroText.appendChild(heroSubtitle);
+
+  const heroButton = document.createElement("button");
+  heroButton.textContent = "VIEW MENU";
+
+  heroContainer.appendChild(heroText);
+  heroContainer.append(heroButton);
 
   return heroContainer;
 }
