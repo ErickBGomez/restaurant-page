9c85144c9159851f3c996f5e632134bbd11084c0
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-10-02 17:42:59 -0600

    Create menu script

---
+++ src/pages/menu.js
@@ -0,0 +1,5 @@
+export default function renderMenu() {
+  const menu = document.createElement("div");
+
+  return menu;
+}
--- src/sections/content.js
+++ src/sections/content.js
@@ -1,10 +1,12 @@
 import renderHome from "../pages/home.js";
+import renderMenu from "../pages/menu.js";
 
 export default function renderContent() {
   const content = document.createElement("div");
   content.id = "content";
 
-  content.appendChild(renderHome());
+  // content.appendChild(renderHome());
+  content.appendChild(renderMenu());
 
   document.body.appendChild(content);
 }
