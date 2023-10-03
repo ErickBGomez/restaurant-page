7186e32310c3b5b41e1beef8026bb1f9729e9497
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-10-02 18:31:50 -0600

    Create dish container structure

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,4 +1,52 @@
-function addTab(tabLabel) {
+function addDishCard(title, price) {
+  const card = document.createElement("div");
+  card.className = "dish-card";
+
+  const image = document.createElement("div");
+  image.className = "dish-image";
+
+  const infoContainer = document.createElement("div");
+  infoContainer.className = "info";
+
+  const cardTitle = document.createElement("div");
+  cardTitle.className = "title";
+  cardTitle.textContent = title;
+
+  const cardPrice = document.createElement("div");
+  cardPrice.className = "price";
+  cardPrice.textContent = price;
+
+  const addCartButton = document.createElement("button");
+  addCartButton.className = "add-cart";
+  addCartButton.textContent = "Add To Cart";
+
+  infoContainer.appendChild(cardTitle);
+  infoContainer.appendChild(cardPrice);
+  infoContainer.appendChild(addCartButton);
+
+  card.appendChild(image);
+  card.appendChild(infoContainer);
+
+  return card;
+}
+
+function createDishContainer() {
+  const container = document.createElement("div");
+  container.className = "dish-container";
+
+  const dishGrid = document.createElement("div");
+  dishGrid.className = "dish-grid";
+
+  for (let i = 0; i < 6; i++) {
+    dishGrid.appendChild(addDishCard("Dish title", "$8.99"));
+  }
+
+  container.appendChild(dishGrid);
+
+  return container;
+}
+
+function addMenuTab(tabLabel) {
   const tab = document.createElement("div");
   tab.className = "tab";
   tab.textContent = tabLabel;
@@ -10,14 +58,12 @@ function createMenuTabs() {
   const tabs = document.createElement("div");
   tabs.className = "menu-tabs";
 
-  const allTab = document.createElement("div");
-
-  tabs.appendChild(addTab("All"));
-  tabs.appendChild(addTab("Breakfast"));
-  tabs.appendChild(addTab("Lunch"));
-  tabs.appendChild(addTab("Dinner"));
-  tabs.appendChild(addTab("Desserts"));
-  tabs.appendChild(addTab("Drinks"));
+  tabs.appendChild(addMenuTab("All"));
+  tabs.appendChild(addMenuTab("Breakfast"));
+  tabs.appendChild(addMenuTab("Lunch"));
+  tabs.appendChild(addMenuTab("Dinner"));
+  tabs.appendChild(addMenuTab("Desserts"));
+  tabs.appendChild(addMenuTab("Drinks"));
 
   return tabs;
 }
@@ -27,6 +73,7 @@ export default function renderMenu() {
   menu.id = "menu";
 
   menu.appendChild(createMenuTabs());
+  menu.appendChild(createDishContainer());
 
   return menu;
 }
